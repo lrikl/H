b78860e96ac038850f51fe192ff37dd7a07e790a
lrikl <lgalahad88@gmail.com>
2021-11-08 13:11:12 +0300

    1.1

--- dz18/js/script.js
+++ dz18/js/script.js
@@ -1,27 +1,27 @@
 function mainFunction(cb) {
-    const a = prompt('Введите число');
-    const b = prompt('Введите число'); 
+    const a = +prompt('Введите число');
+    const b = +prompt('Введите число'); 
     return cb(a, b);
 }
 
 function exponentiation(x1, x2) {
-    const res = alert(`${x1} ** ${x2} = ${x1 ** x2}`);
-    return res;
+    const res = x1 ** x2;
+    return alert(res);
 }
 
 function multiplay(x1, x2) {
-    const res = alert(`${x1} * ${x2} = ${x1 * x2}`);
-    return res;
+    const res = x1 * x2;
+    return alert(res);
 }
 
 function division(x1, x2) {
-    const res = alert(`${x1} / ${x2} = ${x1 / x2}`);
-    return res;
+    const res = x1 / x2;
+    return alert(res);
 }
 
 function modulo(x1, x2) {
-    const res = alert(`${x1} % ${x2} = ${x1 % x2}`);
-    return res;
+    const res = x1 % x2;
+    return alert(res);
 }
 
 mainFunction(exponentiation);
