6d056cddd87fb09c1002e829842a79071c942048
lrikl <lgalahad88@gmail.com>
2021-12-02 12:47:16 +0200

    22

---
+++ dz22/js/script.js
@@ -0,0 +1,29 @@
+function counter(numb = 0) {
+    let count = numb;
+
+    this.increase = function() {
+      return count++;
+    }
+
+    this.decrease = function() {
+      return count--;
+    }
+
+    this.value = function() {
+        return count;
+    }
+}
+  
+let res = new counter();
+let res2 = new counter();
+
+console.log(res.increase());
+console.log(res.increase());
+console.log(res.decrease());
+console.log(res.value());
+
+console.log('-------------------');
+
+console.log(res2.increase()); 
+console.log(res2.increase()); 
+console.log(res2.value()); 
