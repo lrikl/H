af2953079ae8b3be1d70fb45446a44ba2fdef6f8
lrikl <lgalahad88@gmail.com>
2021-12-02 17:17:00 +0200

    25

--- dz25/js/script.js
+++ dz25/js/script.js
@@ -2,15 +2,15 @@ const randomNumber = function() {
     const arr = [];
 
     return function() {
-
         for(let i = 0; arr.length < 100; i++) {
+            
             let rNumb = Math.floor(Math.random() * 100) + 1;
 
             if(arr.indexOf(rNumb) > -1) continue;
+
             arr.push(rNumb);
         }
-
-        return arr
+        return arr;
     }
 }();
   
