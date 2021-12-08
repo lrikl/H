cfa7121c1c4b69ae59f9e2155f39c9a9ca98dad8
lrikl <lgalahad88@gmail.com>
2021-12-08 14:35:27 +0200

    22.2

--- dz22/js/script.js
+++ dz22/js/script.js
@@ -1,4 +1,4 @@
-function counter(count = 0) {
+function consider (count = 0) {
 
   return {
     increase() {
@@ -13,15 +13,45 @@ function counter(count = 0) {
   }
 }
   
-const res = counter();
-const res2 = counter();
+const result = consider();
+const result2 = consider();
+
+console.log(result.increase());
+console.log(result.increase());
+console.log(result.decrease());
+console.log(result.value());
+
+console.log(result2.increase()); 
+console.log(result2.increase()); 
+console.log(result2.value());
+
+// с помощью конструктора new 
+console.log('--------------------------------------------');
+
+function Counter(count = 0) {
+
+  this.increase = function() {
+    return count++;
+  }
+
+  this.decrease = function() {
+    return count--;
+  }
+
+  this.value = function() {
+    return count;
+  }
+}
+  
+const res = new Counter();
+const res2 = new Counter();
 
 console.log(res.increase());
 console.log(res.increase());
 console.log(res.decrease());
 console.log(res.value());
 
-
 console.log(res2.increase()); 
 console.log(res2.increase()); 
 console.log(res2.value()); 
+
